34f60d34cbdab510d7a809a44a8b97145b0f6c22
Norman Breau <norman.breau@totalpave.com>
2017-03-21 17:23:22 -0300

    corrected single quotes.

--- src/android/plugin/google/maps/PluginMap.java
+++ src/android/plugin/google/maps/PluginMap.java
@@ -216,11 +216,11 @@ public class PluginMap extends MyPlugin {
     JSONObject bounds = args.getJSONObject(1);
     int padding = args.getInt(1);
 
-    JSONObject northeastData = bounds.getJSONObject('northeast');
-    JSONObject southwestData = bounds.getJSONObject('southwest');
+    JSONObject northeastData = bounds.getJSONObject("northeast");
+    JSONObject southwestData = bounds.getJSONObject("southwest");
 
-    LatLng northeast = new LatLng(northeastData.getDouble('lat'), northeastData.getDouble('lng'));
-    LatLng southwest = new LatLng(southwestData.getDouble('lat'), southwestData.getDouble('lng'));
+    LatLng northeast = new LatLng(northeastData.getDouble("lat"), northeastData.getDouble("lng"));
+    LatLng southwest = new LatLng(southwestData.getDouble("lat"), southwestData.getDouble("lng"));
 
     LatLngBounds latlngBoudns = new LatLngBounds(northeast, southwest);
 
