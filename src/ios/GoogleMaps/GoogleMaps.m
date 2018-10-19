ee9e141d270a03ba47f7d0bb46ddf66a7e685a2b
Tyler Breau <tyler.breau@totalpave.com>
2018-10-19 13:04:22 -0300

    Added fix

--- src/ios/GoogleMaps/GoogleMaps.m
+++ src/ios/GoogleMaps/GoogleMaps.m
@@ -24,7 +24,11 @@
     //[self versionCheck];
 
 
-    self.pluginLayer = [[MyPluginLayer alloc] initWithFrame:self.webView.frame];
+    //self.pluginLayer = [[MyPluginLayer alloc] initWithFrame:self.webView.frame];
+    CGRect viewBounds = [self.webView bounds];
+    viewBounds.size.height = viewBounds.size.height + 20;
+    self.pluginLayer = [[MyPluginLayer alloc] initWithFrame:viewBounds];
+
     self.pluginLayer.backgroundColor = [UIColor whiteColor];
     self.pluginLayer.webView = self.webView;
     self.pluginLayer.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
