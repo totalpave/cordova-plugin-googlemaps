8542ba455462f601ffe00f164c44919682dd7a23
Norman Breau <norman.breau@totalpave.com>
2019-04-01 12:39:55 -0300

    brought back centerToBounds api

--- www/Map.js
+++ www/Map.js
@@ -570,7 +570,7 @@ Map.prototype.centerToBoudns = function(latLngBounds, padding, duration) {
       }
   };
 
-  self.exec.call(self, null, this.errorHandler, this.id, 'centerToBounds', [bounds, padding || 0, duration || 0]), {
+  self.exec.call(self, null, this.errorHandler, this.id, 'centerToBounds', [bounds, padding || 0, duration || 0], {
     sync: true
   });
   return this;
